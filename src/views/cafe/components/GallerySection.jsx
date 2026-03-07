16f74bd00aa1c4eabdad58c24e3679e85125ce46
Sohaib Khaliq <sohaibkhaliq510@gmail.com>
2026-03-07 09:55:44 +0500

    fix: update image URL in GallerySection component

--- src/views/cafe/components/GallerySection.jsx
+++ src/views/cafe/components/GallerySection.jsx
@@ -7,7 +7,7 @@ export default function GallerySection() {
     "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?w=600&q=80",
     "https://images.unsplash.com/photo-1482049016688-2d3e1b311543?w=600&q=80",
     "https://images.unsplash.com/photo-1481070414801-51fd732d7184?w=600&q=80",
-    "https://images.unsplash.com/photo-1484723091791-cdd51a021278?w=600&q=80",
+    "https://images.unsplash.com/photo-1555939594-58d7cb561ad1?w=600&q=80",
     "https://images.unsplash.com/photo-1510398668585-64539e0bd5fd?w=600&q=80",
     "https://images.unsplash.com/photo-1512621776951-a57141f2eefd?w=600&q=80",
   ];
