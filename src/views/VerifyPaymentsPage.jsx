bf5aaeaa4b5922e8d9174418417ca1598ba0dd51
Sohaib Khaliq <sohaibkhaliq510@gmail.com>
2026-05-09 19:03:31 +0500

    feat: add VerifyPaymentsPage for manual online payment verification

--- src/views/VerifyPaymentsPage.jsx
+++ src/views/VerifyPaymentsPage.jsx
@@ -86,6 +86,8 @@ export default function VerifyPaymentsPage() {
                       "bg-orange-100 text-orange-600": payment.payment_method === 'JazzCash',
                       "bg-green-100 text-green-600": payment.payment_method === 'EasyPaisa',
                       "bg-blue-100 text-blue-600": payment.payment_method === 'Stripe',
+                      "bg-gray-100 text-gray-600": payment.payment_method === 'cash',
+                      "bg-purple-100 text-purple-600": payment.payment_method === 'wallet',
                     })}>
                       {payment.payment_method}
                     </span>
